034ae920c6efbd5ac297deffebbe63f8db3f264e
kemal özkan <kemal.ozkan.ozkan@gmail.com>
2021-08-05 12:27:00 +0300

    Yenilikler Verilen km cinsinden verinin açı değerini veren fonksiyon eklendi. > İstenilen derecedeki paraleller arasında ki mesafe hesaplama eklendi.

--- FindDistance.py
+++ FindDistance.py
@@ -2,6 +2,8 @@ import math
 
 
 class FindDistance:
+    WORLD_RADIUS = 6371
+
     @staticmethod
     def find_latitude(x, line):
         y = ((x - line[0]) * (line[1] - line[3]) / (line[0] - line[2])) + line[1]
@@ -9,15 +11,25 @@ class FindDistance:
 
     @staticmethod
     def haversine_algorithm(point0, point1):
-        WORLD_RADIUS = 6371e3
         x1 = point0[0] * math.pi / 180
         x2 = point1[0] * math.pi / 180
         deltaX = (point1[0] - point0[0]) * math.pi / 180
         deltaY = (point1[1] - point0[1]) * math.pi / 180
 
-        a = math.sin(deltaX / 2) * math.sin(deltaX / 2) + math.cos(x1) * math.cos(x2) * math.sin(deltaY / 2) * math.sin(deltaY / 2)
+        a = math.sin(deltaX / 2) * math.sin(deltaX / 2) + math.cos(x1) * math.cos(x2) * math.sin(deltaY / 2) * math.sin(
+            deltaY / 2)
 
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
-        distance = WORLD_RADIUS * c
+        distance = FindDistance.WORLD_RADIUS * 1000 * c
         return distance
+
+    @staticmethod
+    def calc_longtitude_distance(latitude):
+        longtitude_distance = 2 * math.pi * FindDistance.WORLD_RADIUS * math.cos(math.radians(latitude)) / 360
+        return longtitude_distance
+
+    @staticmethod
+    def meter_to_degree(distance, frequency):
+        degree = frequency / distance
+        return degree
--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from TypeConverter import *
 
 
 class Main:
+    LATITUDE_DISTANCE = 111
     line = [0, 0, 0, 0]
     distance = 0
     floatList0 = []
@@ -15,8 +16,8 @@ class Main:
     coordinates = []
 
     def __init__(self):
-        self.kml_files.append(path.join("KMLDosyaları/Hat_380kV_GELIBOLU 380 - UNIMAR GUNEY(1).kml"))
         self.kml_files.append(path.join("KMLDosyaları/Hat_380kV_GELIBOLU 380 - UNIMAR KUZEY.kml"))
+        self.kml_files.append(path.join("KMLDosyaları/Hat_380kV_GELIBOLU 380 - UNIMAR GUNEY(1).kml"))
 
     def main(self):
         # kml dosyalarının içine xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" eklemeyi unutma
@@ -24,10 +25,9 @@ class Main:
         for k in self.kml_files:
             self.roots.append(ReadKml.read(k))
         for r in self.roots:
-            self.coordinates.append(TypeConverter.stringListtoFloatList(
-                str(r.Document.Placemark.MultiGeometry.LineString.coordinates).split(" ")))
-        distance = FindDistance.haversine_algorithm(self.coordinates[0][0], self.coordinates[1][0])
-        print(distance)
+            self.coordinates.append(TypeConverter.stringListtoFloatList(str(r.Document.Placemark.MultiGeometry.LineString.coordinates).split(" ")))
+        self.distance = FindDistance.haversine_algorithm(self.coordinates[0][0], self.coordinates[0][1])
+        print(round(self.distance/1000, 4))
 
 
 if __name__ == "__main__":
